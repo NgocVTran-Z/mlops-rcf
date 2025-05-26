6ad4f971486ca8bc30f2a3c5f800bdba2768a4af
Ngoc Tran <ngoc.tran@softwareone.com>
2025-05-26 03:35:02 +0000

    x8 to x10

--- pipelines/01_preprocessing_kmeans/preprocessing_kmeans.py
+++ pipelines/01_preprocessing_kmeans/preprocessing_kmeans.py
@@ -23,7 +23,7 @@ def main():
         df = pd.read_csv(BytesIO(obj['Body'].read()), header=None)
 
         # Transform
-        df[0] *= 8
+        df[0] *= 10
 
         # Save to local output
         local_output_path = os.path.join(output_prefix, file)
